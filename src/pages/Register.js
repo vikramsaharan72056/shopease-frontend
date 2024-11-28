8f59947c3c792ddcc0446acb36d9e74f8449214e
Piyushwadhwa0123 <Piyushwadhwa0123@gmail.com>
2024-11-28 10:46:50 +0530

    Link correct for register in Login

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -19,7 +19,7 @@ function Register() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center ">
       <div className="w-full max-w-md p-8 bg-white rounded shadow-lg">
         <h2 className="text-2xl font-bold text-center mb-6">Register</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
